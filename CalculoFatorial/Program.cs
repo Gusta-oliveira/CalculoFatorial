34618df1f33f982b2ca66b61c375b44a5c2dc467
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-03 14:05:27 -0300

    Add project files.

---
+++ CalculoFatorial/Program.cs
@@ -0,0 +1,16 @@
+﻿int factor, aux_calc;
+
+Console.Write("Informe numero a ser fatorado: ");
+factor = int.Parse(Console.ReadLine());
+
+aux_calc = factor;
+Console.WriteLine($"Você desejou ver {factor}!");
+Console.Read();
+while (factor > 1)
+{
+    Console.Write($"{factor} X ");
+    aux_calc = aux_calc * (factor - 1);
+    factor--;
+}
+Console.Write($"{factor} = {aux_calc}");
+Console.Read();
